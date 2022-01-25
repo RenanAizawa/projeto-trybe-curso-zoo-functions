7c9eabec7c7dab7bd72b0fe1d25cda103a34c78b
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-01-25 14:20:04 -0500

    retirada de species

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  data.species.find((specie) => specie.name === animal).every((ssss) => ());
 }
 
 module.exports = getAnimalsOlderThan;
