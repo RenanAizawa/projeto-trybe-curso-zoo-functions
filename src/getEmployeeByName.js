be2bf6370b997a634acb08192dcd2ba40720235f
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-01-25 15:12:22 -0500

    Questao 3, resolvida

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return data.employees.find((employee) => employeeName.includes(employee.firstName)
+  || employeeName.includes(employee.lastName));
 }
 
 module.exports = getEmployeeByName;
