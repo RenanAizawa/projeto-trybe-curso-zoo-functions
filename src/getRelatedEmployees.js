ee5225f84e1f1ff0f9229029dca58bf729b33d5e
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-01-26 15:28:47 -0500

    Desisti da 4 questao

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,7 +1,10 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const emploeyer = (people) => people.id === id;
+  return employees.find(emploeyer);
 }
 
 function getRelatedEmployees(managerId) {
@@ -9,3 +12,5 @@ function getRelatedEmployees(managerId) {
 }
 
 module.exports = { isManager, getRelatedEmployees };
+
+console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
