7f3e77bfcded40cc9927d40a6c690affb8f568a7
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-01-27 13:44:20 -0500

    continuo  garrado na Q.4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const countAnimal = data.species
+    .filter((bicho) => ({ bicho: bicho.residents }));
+  console.log(countAnimal);
+  return countAnimal;
 }
-
+countAnimals();
 module.exports = countAnimals;
