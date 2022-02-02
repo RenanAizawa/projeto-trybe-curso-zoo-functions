53a43fa44805966304e06d201e734efec0c7bbf0
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-02-02 16:17:18 -0500

    Q.9, resolvida

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
+const animalCuidado = (id) => employees.find((empregado) => empregado.id === id).responsibleFor[0];
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const arrayAnimais = species.find((animal) => animal.id === animalCuidado(id))
+    .residents.sort((age1, age2) => age2.age - age1.age);
+  return [arrayAnimais[0].name, arrayAnimais[0].sex, arrayAnimais[0].age];
 }
 
 module.exports = getOldestFromFirstSpecies;
