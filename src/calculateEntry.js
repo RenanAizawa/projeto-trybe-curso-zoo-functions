0548518723f9d60bb6f28d40188a60987db53046
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-02-02 15:10:41 -0500

    Q.6 concluida

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,13 +1,29 @@
 const data = require('../data/zoo_data');
 
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
+const { prices } = data;
+
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+const somaChild = (objeto) => {
+  const price = prices.child * objeto.child;
+  return price;
+};
+
+const somaAdult = (objeto) => {
+  const price = prices.adult * objeto.adult;
+  return price;
+};
+
+const somaSenior = (objeto) => {
+  const price = prices.senior * objeto.senior;
+  return price;
+};
 
 const countChild = (para) => {
   const retorno = para.filter((entrant) => entrant.age < 18).length;
@@ -30,14 +46,18 @@ const quantidadePorIdade = (entrant) => ({ child: countChild(entrant),
 
 function countEntrants(entrants) {
   // seu código aqui
-  if (!entrants) {
-    return 0;
-  }
   return quantidadePorIdade(entrants);
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const totalPrice = somaChild(countEntrants(entrants))
+  + somaAdult(countEntrants(entrants))
+  + somaSenior(countEntrants(entrants));
+  return totalPrice;
 }
-
+// calculateEntry(entrants);
 module.exports = { calculateEntry, countEntrants };
